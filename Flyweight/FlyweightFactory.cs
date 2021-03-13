68682b5098080185e3adb20c99f24f0b76209023
soniafriesen <58013701+soniafriesen@users.noreply.github.com>
2021-03-12 20:03:46 -0500

    Add files via upload

---
+++ Flyweight/FlyweightFactory.cs
@@ -0,0 +1,50 @@
+﻿using Assi1.Strategies;
+using System;
+using System.Collections.Generic;
+/*
+ * Project: Assi1 (FlyweightFactory)
+ * Purpose: holds the strategies and inidcates which class to eveluate
+ * Coder: Sonia Friesen, 0813682    
+ * Date: Due February 16, 2021
+ */
+namespace Assi1.Flyweights
+{
+    public class FlyweightFactory
+    {
+        protected Dictionary<string, StackingStrategy> strategies;
+        
+        //constructor
+        public FlyweightFactory()
+        {
+            strategies = new Dictionary<string, StackingStrategy>();
+        }
+        public StackingStrategy GetFlyweight(string type)
+        {
+            StackingStrategy strategy = null;
+            if(strategies.ContainsKey(type))
+            {
+                strategy = strategies[type];
+            }
+            else
+            {
+                switch(type)
+                {
+                    case "bottomWeight":
+                        strategy = new StackingStrategy_BottomWeight();
+                        break;
+                    case "pyramid":
+                        strategy = new StackingStrategy_Pyramid();
+                        break;
+                    case "topple":
+                        strategy = new StackingStrategy_Topple();
+                        break;
+                    default:
+                        throw new ArgumentNullException($"{type} is not a strategy type.");                       
+
+                }
+                strategies[type] = strategy;
+            }
+            return strategy;
+        }
+    }
+}
